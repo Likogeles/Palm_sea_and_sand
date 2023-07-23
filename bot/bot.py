b5965661098409375d9c7d183dc75cddcf0670de
Yana <yana@MacBook-Air-Yana.local>
2023-07-23 19:28:19 +0300

    yana_bot: add geolocation function

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,17 @@
 import logging
+import re
+import json
+from UserDB.User import User
+
+from os import environ
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+API_TOKEN = environ.get('API_TOKEN')
 bot = Bot(API_TOKEN)
+user = User()
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -13,19 +19,53 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+# Addiing user_data, keyboard with different time and flag for location tracking
+
 user_data = []
+kb = [
+    [types.KeyboardButton(text="0:00"),
+     types.KeyboardButton(text="1:00"),
+     types.KeyboardButton(text="2:00")],
+    [types.KeyboardButton(text="3:00"),
+     types.KeyboardButton(text="4:00"),
+     types.KeyboardButton(text="5:00")],
+    [types.KeyboardButton(text="6:00"),
+     types.KeyboardButton(text="7:00"),
+     types.KeyboardButton(text="8:00")],
+    [types.KeyboardButton(text="9:00"),
+     types.KeyboardButton(text="10:00"),
+     types.KeyboardButton(text="11:00")],
+    [types.KeyboardButton(text="12:00"),
+     types.KeyboardButton(text="13:00"),
+     types.KeyboardButton(text="14:00")],
+    [types.KeyboardButton(text="15:00"),
+     types.KeyboardButton(text="16:00"),
+     types.KeyboardButton(text="17:00")],
+    [types.KeyboardButton(text="18:00"),
+     types.KeyboardButton(text="19:00"),
+     types.KeyboardButton(text="20:00")],
+    [types.KeyboardButton(text="21:00"),
+     types.KeyboardButton(text="22:00"),
+     types.KeyboardButton(text="23:00")
+    ],
+]
+keyboard = types.ReplyKeyboardMarkup(
+    keyboard=kb,
+    resize_keyboard=True,
+)
+flag = False
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
 
-    kb = [
+    keyb = [
         [
             types.KeyboardButton(text="Маршрут"),
             types.KeyboardButton(text="Анкета")
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
+        keyboard=keyb,
         resize_keyboard=True,
         input_field_placeholder="Выберите, что вы хотите"
     )
@@ -37,44 +77,36 @@ async def send_welcome(message: types.Message):
     await bot.send_sticker(message.from_user.id, sticker = "CAACAgIAAxkBAAEJwlpkun7HJX19BUAerEIc3G7jVD4RjgACrRgAAnmFiUi3haSlMSLa5S8E")
 
 @dp.message_handler(text=["Анкета"])
-async def start_form(message: types.Message):
+async def geolocation(message: types.Message):
+    await message.answer("Введите адрес пребывания или отправьте геопозицию", reply_markup=types.ReplyKeyboardRemove())
 
-    kb = [
-        [
-            types.KeyboardButton(text="0:00"),
-            types.KeyboardButton(text="1:00"),
-            types.KeyboardButton(text="2:00")],
-            [types.KeyboardButton(text="3:00"),
-            types.KeyboardButton(text="4:00"),
-            types.KeyboardButton(text="5:00")],
-            [types.KeyboardButton(text="6:00"),
-            types.KeyboardButton(text="7:00"),
-            types.KeyboardButton(text="8:00")],
-            [types.KeyboardButton(text="9:00"),
-            types.KeyboardButton(text="10:00"),
-            types.KeyboardButton(text="11:00")],
-            [types.KeyboardButton(text="12:00"),
-            types.KeyboardButton(text="13:00"),
-            types.KeyboardButton(text="14:00")],
-            [types.KeyboardButton(text="15:00"),
-            types.KeyboardButton(text="16:00"),
-            types.KeyboardButton(text="17:00")],
-            [types.KeyboardButton(text="18:00"),
-            types.KeyboardButton(text="19:00"),
-            types.KeyboardButton(text="20:00")],
-            [types.KeyboardButton(text="21:00"),
-            types.KeyboardButton(text="22:00"),
-            types.KeyboardButton(text="23:00")
-        ],
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-    )
+@dp.message_handler(content_types=['location'])
+async def handle_loc(message):
+    lat = message.location['latitude']
+    lon = message.location['longitude']
+    user.place = (lat, lon)
+    print(user.places)
+
+@dp.message_handler(text=["Лоооол"])
+async def arrival_time(message: types.Message):
+    await message.answer("Когда вы прибываете в город?", reply_markup=keyboard)
+
+@dp.message_handler()
+def message_accept(message: types.Message):
+
+    # putting arrival time in user's data
     
-    await message.answer("Когда вы пребываете в город?", reply_markup=keyboard)
-    user_data.append(text);
+    result = re.fullmatch(r'\d{1,2}:\d\d', message.text)
+    if result:
+        user_data.append(message.text)
+        message.reply("")
+        
+    
+    
 
+
+    
+    # await bot.send_message(msg.from_user.id, msg.text)
     
 
 """
@@ -143,7 +175,8 @@ async def robot_question(call: types.CallbackQuery):
             user_data.append("aged")
         if call.data == "ancient":
             user_data.append("ancient")
-        text = "Спасибо за ответы, можете переходить к построению маршрута"
+        text = "ты лох"
+        chat_id = message.chat.id
         await bot.send_message(chat_id = chat_id, text = text)
 """
 
