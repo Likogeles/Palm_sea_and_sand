59f09f1686d8eb218e15e0b184b6f34455879fb7
yagrinevich <116907976+yagrinevich@users.noreply.github.com>
2023-07-23 18:29:12 +0300

    Add files via upload

---
+++ bot/TomasBot.py
@@ -0,0 +1,179 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+API_TOKEN = "6373726278:AAEHzQ4fbhWHHyvMb1oPH9JfdzDhS2bIHlE"
+bot = Bot(API_TOKEN)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+user_data = []
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Маршрут"),
+            types.KeyboardButton(text="Анкета")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите, что вы хотите"
+    )
+
+    # This handler will be called when user sends `/start` or `/help` command
+
+    await message.reply("Привет!\nЯ бот, который поможет проложить маршрут", reply_markup=keyboard)
+    await bot.send_sticker(message.from_user.id,
+                           sticker="CAACAgIAAxkBAAEJwlpkun7HJX19BUAerEIc3G7jVD4RjgACrRgAAnmFiUi3haSlMSLa5S8E")
+
+
+@dp.message_handler(text=["Анкета"])
+async def start_form(message: types.Message):
+    btn1 = InlineKeyboardButton(text="кино", callback_data="answer_cinema")
+    btn2 = InlineKeyboardButton(text="театр", callback_data="answer_theatre")
+    keyboard_inline = InlineKeyboardMarkup().add(btn1, btn2)
+    await message.answer("Тебе нравится кино или театр?", reply_markup=keyboard_inline)
+
+
+@dp.message_handler(text=["Маршрут"])
+async def start_form(message: types.Message):
+    btn_prior_1 = InlineKeyboardButton(text="Кино", callback_data="prior_cinema")
+    btn_prior_2 = InlineKeyboardButton(text="Театр", callback_data="prior_theatre")
+    btn_prior_3 = InlineKeyboardButton(text="Музей", callback_data="prior_museum")
+    btn_prior_4 = InlineKeyboardButton(text="Бар", callback_data="prior_bar")
+    btn_prior_5 = InlineKeyboardButton(text="Клуб", callback_data="prior_club")
+    keyboard_inline = InlineKeyboardMarkup()
+    keyboard_inline.add(btn_prior_1)
+    keyboard_inline.add(btn_prior_2)
+    keyboard_inline.add(btn_prior_3)
+    keyboard_inline.add(btn_prior_4)
+    keyboard_inline.add(btn_prior_5)
+    await message.answer("Выбери в порядке приоритета:", reply_markup=keyboard_inline)
+
+arr_answer = []
+
+
+@dp.callback_query_handler(text=["prior_cinema", "prior_theatre", "prior_museum", "prior_bar", "prior_club"])
+async def track_question(call: types.CallbackQuery):
+    if call.data == "prior_cinema" or call.data == "prior_theatre" or call.data == "prior_museum" or call.data == "prior_bar" or call.data == "prior_club":
+        if len(arr_answer) < 5:
+            arr_answer.append(call.data)
+            if len(arr_answer) == 5:
+                await call.message.answer("Итоговый приоритет: " + str(arr_answer))
+            else:
+                await call.message.answer("В порядке приоритета: " + str(arr_answer))
+
+
+@dp.callback_query_handler(
+    text=["answer_cinema", "answer_theatre", "history_yes", "history_no", "vegan_yes", "vegan_no", "sugar_yes",
+          "sugar_no", "teenage", "young", "adult", "aged", "ancient", "answer_bar", "answer_club", "avto", "hiking",
+        "activ_yes", "activ_no", "art_yes", "art_no",  ])
+async def resume_question(call: types.CallbackQuery):
+    print(call.data)
+    # Тебе нравится кино или театр?
+
+    if call.data == "answer_cinema" or call.data == "answer_theatre":
+        if call.data == "answer_cinema":
+            user_data.append("кино")
+        else:
+            user_data.append("театр")
+        btn1 = InlineKeyboardButton(text="Да", callback_data="history_yes")
+        btn2 = InlineKeyboardButton(text="Нет", callback_data="history_no")
+        keyboard_inline = InlineKeyboardMarkup().add(btn1, btn2)
+        await call.message.answer("Тебе нравится история?", reply_markup=keyboard_inline)
+
+    # Тебе нравится история?
+
+    elif call.data == "history_yes" or call.data == "history_no":
+        if call.data == "history_yes":
+            user_data.append("Да")
+        else:
+            user_data.append("Нет")
+        btn1 = InlineKeyboardButton(text="Мне 15-35 лет", callback_data="teenage")
+        btn2 = InlineKeyboardButton(text="Мне 35-35 лет", callback_data="young")
+        btn3 = InlineKeyboardButton(text="Мне 55 и более", callback_data="adult")
+        #btn4 = InlineKeyboardButton(text="Мне 45-55 лет", callback_data="aged")
+        #btn5 = InlineKeyboardButton(text="Мне 65+ лет", callback_data="ancient")
+        keyboard_inline = InlineKeyboardMarkup()
+        keyboard_inline.add(btn1)
+        keyboard_inline.add(btn2)
+        keyboard_inline.add(btn3)
+        #keyboard_inline.add(btn4)
+        #keyboard_inline.add(btn5)
+        await call.message.answer("Сколько тебе лет?", reply_markup=keyboard_inline)
+
+    if call.data == "teenage" or call.data == "young" or call.data == "adult": #or call.data == "aged" or call.data == "ancient":
+        if call.data == "teenage":
+            user_data.append("teenage")
+        elif call.data == "young":
+            user_data.append("young")
+        elif call.data == "adult":
+            user_data.append("adult")
+        #elif call.data == "aged":
+            #user_data.append("aged")
+        #elif call.data == "ancient":
+            #user_data.append("ancient")
+        btn1 = InlineKeyboardButton(text="Да", callback_data="activ_yes")
+        btn2 = InlineKeyboardButton(text="Нет", callback_data="activ_no")
+        keyboard_inline = InlineKeyboardMarkup().add(btn1, btn2)
+        await call.message.answer("Ты любишь активный отдых?", reply_markup=keyboard_inline)
+
+
+     # Ты любишь искусство
+
+    elif call.data == "activ_yes" or call.data == "activ_no":
+        if call.data == "activ_yes":
+            user_data.append("Да")
+        else:
+            user_data.append("Нет")
+        btn1 = InlineKeyboardButton(text="Да", callback_data="art_yes")
+        btn2 = InlineKeyboardButton(text="Нет", callback_data="art_no")
+        keyboard_inline = InlineKeyboardMarkup().add(btn1, btn2)
+        await call.message.answer("Ты любишь искусство?", reply_markup=keyboard_inline)
+    #Ты любишь активный отдых?
+
+    elif call.data == "art_yes" or call.data == "art_no":
+        if call.data == "art_yes":
+            user_data.append("Да")
+        else:
+            user_data.append("Нет")
+        btn1 = InlineKeyboardButton(text="На транспорте", callback_data="avto")
+        btn2 = InlineKeyboardButton(text="Пешком", callback_data="hiking")
+        keyboard_inline = InlineKeyboardMarkup().add(btn1, btn2)
+        await call.message.answer("Как ты любишь передвигаться по городу?", reply_markup=keyboard_inline)
+
+    #Ты выберешь отдых на природе или в городе
+
+    elif call.data == "avto" or call.data == "hiking":
+        if call.data == "avto":
+            user_data.append("На авто :)")
+        else:
+            user_data.append("Пешком")
+        btn1 = InlineKeyboardButton(text="Авантюрный", callback_data="advanture")
+        btn2 = InlineKeyboardButton(text="Спокойный", callback_data="calm")
+        keyboard_inline = InlineKeyboardMarkup().add(btn1, btn2)
+        await call.message.answer("Ты авантюрный или спокойный", reply_markup=keyboard_inline)
+
+    #Ты авантюрный или спокойный
+
+    elif call.data == "advanture" or call.data == "calm":
+        if call.data == "advanture":
+            user_data.append("Авантюрный")
+        else:
+            user_data.append("Спокойный")
+        await call.message.answer("Спасибо, можете переходить к составлению маршрута")
+
+# executor.start_polling(dp)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
